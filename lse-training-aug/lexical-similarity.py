f981588ded78d92b351bec32b0b6975c5e8c635b
Euan McGill <mcgilleuan@icloud.com>
2021-06-09 13:31:15 +0200

    lex sim script define

--- lse-training-aug/lexical-similarity.py
+++ lse-training-aug/lexical-similarity.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import re
+
+lse_corpus = '/Users/e.mcgill/Documents/upf/corpora/UPM-LSE/BD/TEXTOS/signos_train.txt'
+# maybe use .lower() with this
+spanish_corpus = '/Users/e.mcgill/Documents/upf/corpora/UPM-LSE/BD/TEXTOS/frases_train.txt'
+
+print(set(lse_corpus))
