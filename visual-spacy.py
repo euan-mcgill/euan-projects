fba030c1d96cc0ed745082f29352c073d4998b2a
Euan McGill <mcgilleuan@icloud.com>
2022-02-09 11:25:40 +0100

    8th feb

--- visual-spacy.py
+++ visual-spacy.py
@@ -4,8 +4,8 @@
 import spacy
 from spacy import displacy
 
-nlp = spacy.load("en_core_web_sm")
-doc = nlp("Shippea Hill is located seven miles in an easterly bearing from Ely, Cambridgeshire")
+nlp = spacy.load("es_core_news_lg")
+doc = nlp("cuando vuelvas a vivir aquí tendrás que traer un certificado del padrón municipal al renovarte el denei")
 displacy.serve(doc, style="dep")
 
 # to close process:
