0007f7b7eb9ee3391519e4208849d6e761bb4f19
Euan McGill <mcgilleuan@icloud.com>
2022-03-17 21:24:23 +0100

    ordering rules good

--- rule-aug/ordering_rules.py
+++ rule-aug/ordering_rules.py
@@ -11,175 +11,73 @@ import re
 import spacy as sp
 
 model_load = importLM()
-doc = model_load.to_doc("con tu denei puedes viajar a estos países")
+# doc = model_load.to_doc("vivo en este país para cada tres días de la semana y tu padre")
+doc = model_load.to_doc("si no vives en españa tienes que traer estos documentos")
 
 #%%
 
 '''
 NPs: NOUN + DEMONSTRATIVE + POSSESSIVE + NUMERAL + INDEFINITE
-    NOUN = nsubj NOUN
-         = ROOT NOUN
-         = obl NOUN
-         = obj NOUN
-    DEMONSTRATIVE = det DET
-    POSSESSIVE = det DET
-    NUMERAL = nummod NUM
-            = compound NUM
-    INDEFINITE = det DET
-    det nsubj -> nsubj det
-    nummod nsubj -> nsubj nummod
-    det nummod nsubj -> nsubj det nummod (DEM and POSS)
-    det nummod nsubj ->  nsubj nummod det (INDEF)
-    https://universaldependencies.org/treebanks/es_pud/index.html
+
+
+https://universaldependencies.org/treebanks/es_pud/index.html
 '''
 
 def ord_prep(doc):
     listag = []
     deptag = []
     lemtag = []
+    postag = []
     for token in doc:
         # get tuples of spaCy tags (combined and individual)
-        tagged = (token.lemma_,token.dep_)
+        tagged = (token.lemma_,token.pos_)
         tags_only = (token.dep_)
         lemmas_only = (token.lemma_)
+        pos_only = (token.pos_)
         # merge lemma and dep tag together as one lexical item
         combine = '_'.join(tagged)
         # list each type of transcription, then convert to string for analysis
         listag.append(combine)
         deptag.append(tags_only)
         lemtag.append(lemmas_only)
+        postag.append(pos_only)
         tagstring = ' '.join(listag)
         depstring = ' '.join(deptag) # only return this one in prep?
         lemstring = ' '.join(lemtag)
-    return tagstring, depstring, lemstring
+        posstring = ' '.join(postag)
+    return tagstring, depstring, lemstring, posstring
 
-def np_ord_rules(depstring,tagstring): # split into noun chunks first and then reorder?
-#
-# order is very important (if loops seem a little messy or unstable)
-# why not remove all det=articles first? And use pos
-#
-    ### N + NUM + INDEF ###
-    if ("det nummod obl" in depstring and "cada_det" in tagstring or
-        "todo_det" in tagstring or "mismo_det" in tagstring or "otro_det" in
-        tagstring or "mucho_det" in tagstring or "tanto_det" in tagstring
-        or "varios_det" in tagstring):
-        num = re.sub(r'(\S+_det)(\s)(\S+_nummod)(\s)(\S+_obl)',r'\5\4\3\2\1',tagstring)
-    elif ("det nummod nsubj" in depstring and "cada_det" in tagstring or
-          "todo_det" in tagstring or "mismo_det" in tagstring or "otro_det" in
-          tagstring or "mucho_det" in tagstring or "tanto_det" in tagstring
-          or "varios_det" in tagstring):
-        num = re.sub(r'(\S+_det)(\s)(\S+_nummod)(\s)(\S+_nsubj)',r'\5\4\3\2\1',tagstring)
-    elif ("det nummod obj" in depstring and "cada_det" in tagstring or
-          "todo_det" in tagstring or "mismo_det" in tagstring or "otro_det" in
-          tagstring or "mucho_det" in tagstring or "tanto_det" in tagstring
-          or "varios_det" in tagstring):
-        num = re.sub(r'(\S+_det)(\s)(\S+_nummod)(\s)(\S+_obj)',r'\5\4\3\2\1',tagstring)
-    ### N + DEM + NUM ###
-    elif ("det nummod obl" in depstring and "este_det" in tagstring or "ese_det" in
-          tagstring or "aquel_det" in tagstring or "alguno_det" in tagstring):
-        num = re.sub(r'(\S+_det)(\s)(\S+_nummod)(\s)(\S+_obl)',r'\5\4\1\2\3',tagstring)
-    ### N + POSS + INDEF ### (amod det obj, det det obl)
-    ### N + INDEF ###
-    elif ("det obl" in depstring and "cada_det" in tagstring or
-          "todo_det" in tagstring or "mismo_det" in tagstring or "otro_det" in
-          tagstring or "mucho_det" in tagstring or "tanto_det" in tagstring
-          or "varios_det" in tagstring):
-        num = re.sub(r'(\S+_det)(\s)(\S+_obl)',r'\3\2\1',tagstring)
-    elif ("det ROOT" in depstring and "cada_det" in tagstring or
-          "todo_det" in tagstring or "mismo_det" in tagstring or "otro_det" in
-          tagstring or "mucho_det" in tagstring or "tanto_det" in tagstring
-          or "varios_det" in tagstring):
-        num = re.sub(r'(\S+_det)(\s)(\S+_ROOT)',r'\3\2\1',tagstring)
-    ### N + DEM ###
-    elif ("det nsubj" in depstring and "este_det" in tagstring or "ese_det" in
-          tagstring or "aquel_det" in tagstring or "alguno_det" in tagstring):
-        num = re.sub(r'(\S+_det)(\s)(\S+_nsubj)',r'\3\2\1',tagstring)
-    elif ("det obl" in depstring and "este_det" in tagstring or "ese_det" in
-          tagstring or "aquel_det" in tagstring or "alguno_det" in tagstring):
-        num = re.sub(r'(\S+_det)(\s)(\S+_obl)',r'\3\2\1',tagstring)
-    ### N + NUM ###
-    elif "nummod obl" in depstring:
-        num = re.sub(r'(\S+_nummod)(\s)(\S+_obl)',r'\3\2\1',tagstring)
-    elif "nummod obj" in depstring:
-        num = re.sub(r'(\S+_nummod)(\s)(\S+_obj)',r'\3\2\1',tagstring)
-    elif "nummod nsubj" in depstring:
-        num = re.sub(r'(\S+_nummod)(\s)(\S+_nsubj)',r'\3\2\1',tagstring)
-    return num, depstring
-
-# NOW, remove all the rest of the dets
-
-def np_ord_cascade():
-    # change to loop/iter for multiple lines
-    pre_deps = ord_prep(doc)[1]
-    pre_tags = ord_prep(doc)[0] # lemstring avail. if you need to retain full word forms
-    tagstring = np_ord_rules(pre_deps,pre_tags)[0]
-    depstring = re.sub(r'\S+_',r'',tagstring)
-    # x = np_ord_rules(deps_again,tags_again) # UnboundLocalError: local variable 'num' referenced before assignment
-    # return x
-    # now repeat rule loop here (maybe a third time if nec.)
-    ### N + NUM + INDEF ###
-    if ("det nummod obl" in depstring and "cada_det" in tagstring or
-        "todo_det" in tagstring or "mismo_det" in tagstring or "otro_det" in
-        tagstring or "mucho_det" in tagstring or "tanto_det" in tagstring
-        or "varios_det" in tagstring):
-        sec = re.sub(r'(\S+_det)(\s)(\S+_nummod)(\s)(\S+_obl)',r'\5\4\3\2\1',tagstring)
-    elif ("det nummod nsubj" in depstring and "cada_det" in tagstring or
-          "todo_det" in tagstring or "mismo_det" in tagstring or "otro_det" in
-          tagstring or "mucho_det" in tagstring or "tanto_det" in tagstring
-          or "varios_det" in tagstring):
-        sec = re.sub(r'(\S+_det)(\s)(\S+_nummod)(\s)(\S+_nsubj)',r'\5\4\3\2\1',tagstring)
-    elif ("det nummod obj" in depstring and "cada_det" in tagstring or
-          "todo_det" in tagstring or "mismo_det" in tagstring or "otro_det" in
-          tagstring or "mucho_det" in tagstring or "tanto_det" in tagstring
-          or "varios_det" in tagstring):
-        sec = re.sub(r'(\S+_det)(\s)(\S+_nummod)(\s)(\S+_obj)',r'\5\4\3\2\1',tagstring)
-    ### N + DEM + NUM ###
-    elif ("det nummod obl" in depstring and "este_det" in tagstring or "ese_det" in
-          tagstring or "aquel_det" in tagstring or "alguno_det" in tagstring):
-        sec = re.sub(r'(\S+_det)(\s)(\S+_nummod)(\s)(\S+_obl)',r'\5\4\1\2\3',tagstring)
-    ### N + POSS + INDEF ### (amod det obj, det det obl)
-    ### N + INDEF ###
-    elif ("det obl" in depstring and "cada_det" in tagstring or
-          "todo_det" in tagstring or "mismo_det" in tagstring or "otro_det" in
-          tagstring or "mucho_det" in tagstring or "tanto_det" in tagstring
-          or "varios_det" in tagstring):
-        sec = re.sub(r'(\S+_det)(\s)(\S+_obl)',r'\3\2\1',tagstring)
-    elif ("det ROOT" in depstring and "cada_det" in tagstring or
-          "todo_det" in tagstring or "mismo_det" in tagstring or "otro_det" in
-          tagstring or "mucho_det" in tagstring or "tanto_det" in tagstring
-          or "varios_det" in tagstring):
-        sec = re.sub(r'(\S+_det)(\s)(\S+_ROOT)',r'\3\2\1',tagstring)
-    ### N + DEM ###
-    elif ("det nsubj" in depstring and "este_det" in tagstring or "ese_det" in
-          tagstring or "aquel_det" in tagstring or "alguno_det" in tagstring):
-        sec = re.sub(r'(\S+_det)(\s)(\S+_nsubj)',r'\3\2\1',tagstring)
-    elif ("det obl" in depstring and "este_det" in tagstring or "ese_det" in
-          tagstring or "aquel_det" in tagstring or "alguno_det" in tagstring):
-        sec = re.sub(r'(\S+_det)(\s)(\S+_obl)',r'\3\2\1',tagstring)
-    ### N + NUM ###
-    elif "nummod obl" in depstring:
-        sec = re.sub(r'(\S+_nummod)(\s)(\S+_obl)',r'\3\2\1',tagstring)
-    elif "nummod obj" in depstring:
-        sec = re.sub(r'(\S+_nummod)(\s)(\S+_obj)',r'\3\2\1',tagstring)
-    elif "nummod nsubj" in depstring:
-        sec = re.sub(r'(\S+_nummod)(\s)(\S+_nsubj)',r'\3\2\1',tagstring)
-    return sec, depstring
-
-'''
-Possessive constructions (might be able to include in np_ord)
-'''
-def possessive_const():
-    pass
-    # DET (subset, but could combine with set of rules above) + nsubj/obl/obj
-    # case nmod
-    # output = N + PROPIO + PRON
-    ### incorporate removing all DETs here? ###
+# mortag = []
+# for token in doc:
+#     morphed = (token.lemma_, ' '.join(token.morph.get("Person")), ' '.join(token.morph.get("Number")))
+#     together = '_'.join(morphed)
+#     mortag.append(together)
+#     morstring = ' '.join(mortag)
 
 def compulsory_subj(doc):
-    # this should come before reordering rules
-    morphtag = []
-    for token in doc:
-        tagged = (token.text,token.morph)
+    # must also consider negatives w/in the word order
+    # tienes que -> necesitar
+    pass
+
+def np_ord(tag,pos):
+    tag = ord_prep(doc)[0]
+    pos = ord_prep(doc)[3]
+    #
+    # first remove DETs which are (in)definite articles
+    a = re.sub(r'el_DET ',r'',tag)
+    # move all DETs before NOUNs
+    b = re.sub(r'(\S+_DET)(\s+)(\S+_NOUN)',r'\3\2\1',a)
+    # modify tags for INDEF, POSS and DEM
+    c = re.sub(r'(este|ese|aquel|alguno)(_DET)',r'\1_DEM',b)
+    d = re.sub(r'(mi|tu|su|nuestro|vuestro)(_DET)',r'\1_PSS',c)
+    e = re.sub(r'(cada|todo|mismo|otro|mucho|tanto|varios)(_DET)',r'\1_IDF',d)
+    # all DEM + NOUNs are now flipped, 3 combos only, first DEM NUM NOUN -> NOUN DEM NUM
+    f = re.sub(r'(\S+_DEM)(\s)(\S+_NUM)(\s)(\S+_NOUN)',r'\5\4\1\2\3',e)
+    # now INDEF NUM NOUN -> NOUN NUM INDEF
+    g = re.sub(r'(\S+_IDF)(\s)(\S+_NUM)(\s)(\S+_NOUN)',r'\5\4\3\2\1',f)
+    # now possessive constructions
+    h = re.sub(r'(\S+_NOUN)(\s)(\S+_PSS)',r'\1\2PROPIO\2\3',g)
+    return h
 
 def plural_constructions(doc):
     # this should come before reordering rules
@@ -193,6 +91,14 @@ Alternative algorithm (a la Vegas Cañas):
     Label
     If they match conditional word order, plug them into sentence
 '''
+
+#%%
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
 
 
 
