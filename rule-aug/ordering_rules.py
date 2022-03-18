e0d1adbdb784d05f2a155c6a4cf836c766690638
Euan McGill <mcgilleuan@icloud.com>
2022-03-18 18:59:24 +0100

    CHECKPOINT - fullrules minus tense marking

--- rule-aug/ordering_rules.py
+++ rule-aug/ordering_rules.py
@@ -11,16 +11,21 @@ Rules covered:
     Compulsory subject
     Movement of ADVs after the verb
     No articles
+    No adpositions (might need to alter to accept 'para')
+    Possessive constructions
+    Mark plurals with "PLURAL" marker
 """
 
 from model_init import importLM
 import re
 import spacy as sp
+import sys
 
-model_load = importLM()
-# doc = model_load.to_doc("vivo en este país para cada tres días de la semana y tu padre")
-doc = model_load.to_doc("una fotocopia del denei también vale si no tienes el original")
-# doc = model_load.to_doc("una fotocopia del denei no vale si tú no tienes el original")
+# model_load = importLM()
+# doc = model_load.to_doc("de los treinta a los setenta años se renovará cada diez años")
+# testprondoc = model_load.to_doc("debes recoger el denei en el mismo sitio donde tú lo solicistaste")
+# doc = model_load.to_doc("una fotocopia del denei también vale si no tienes el original")
+# doc = model_load.to_doc("una fotocopia del denei ya no vale si tú no tienes el original")
 
 #%%
 
@@ -45,7 +50,8 @@ def ord_prep(doc):
         pos_only = (token.pos_)
         pernum = (token.lemma_, token.pos_,
                   ' '.join(token.morph.get("Person")),
-                  ' '.join(token.morph.get("Number")))
+                  ' '.join(token.morph.get("Number")),
+                  ' '.join(token.morph.get("Case")))
         # merge lemma and pos tag together as one lexical item, merge verb inflection info
         combine = '_'.join(tagged)
         inflect = '_'.join(pernum)
@@ -62,27 +68,43 @@ def ord_prep(doc):
         morstring = ' '.join(mortag)
     return tagstring, depstring, lemstring, posstring, morstring
 
-def compulsory_subj():
+def compulsory_subj(mor):
     # delete ser/estar before or after this stage
     # must also consider negatives w/in the word order
     # tienes que -> necesitar
     # another edge case - reflexive verbs (check)
-    tag = ord_prep(doc)[0]
-    dep = ord_prep(doc)[1]
-    pos = ord_prep(doc)[3]
-    mor = ord_prep(doc)[4]
+    # mor = ord_prep(doc)[4]
     # Adverbs go after the verb apart from "no" or "ya no". Move other adverbs
-    alpha = re.sub(r'ya_ADV__', r'ya_YA__',mor)
-    bravo = re.sub(r'no_ADV__', r'no_NEG__',alpha)
-    charlie = re.sub(r'(\S+_ADV\S+)(\s)(\S+_VERB\S+)',r'\3\2\1',bravo)
+    alpha = re.sub(r'ya_ADV___', r'ya_YA___',mor)
+    bravo = re.sub(r'no_ADV___', r'no_NEG___',alpha)
+    bc = re.sub(r' Dat\b',r'',bravo)
+    charlie = re.sub(r'(\S+_ADV\S+)(\s)(\S+_(VERB|AUX)\S+)',r'\3\2\1',bc)
     # now to the pronouns
-    delta = re.sub(r'(\S+VERB_1_Sing)',r'',charlie)
+    delta = re.sub(r'(\S+(VERB|AUX)_1_Sing_)',r'yo_PRON_1_Sing_Nom \1',charlie)
+    echo = re.sub(r'(\S+(VERB|AUX)_2_Sing_)',r'tú_PRON_2_Sing_Nom \1',delta)
+    # something to deal with ella, ellas
+    foxtrot = re.sub(r'(\S+(VERB|AUX)_3_(Sing|Plur)_)',r'él_PRON_3_Sing_Nom \1',echo)
+    golf = re.sub(r'(\S+(VERB|AUX)_1_Plur_)',r'nuestro_PRON_1_Sing_Nom \1',foxtrot)
+    hotel = re.sub(r'(\S+(VERB|AUX)_2_Plur_)',r'vuestro_PRON_2_Sing_Nom \1',golf)
+    # now move pron before ya no then no
+    india = re.sub(r'(\S+NEG\S+)(\s)(\S+PRON\S+)',r'\3\2\1',hotel)
+    juliett = re.sub(r'(\S+YA\S+)(\s)(\S+PRON\S+)',r'\3\2\1',india)
+    # double prons, get rid of acc prons
+    kilo = re.sub(r'(\S+PRON\S+_Acc)',r'',juliett)
+    lima = re.sub(r'(\S+PRON\S+_Nom)(\s+)(\S+PRON\S+_Nom)',r'\1',kilo)
+    # future/past tense also here
+    # what should we do with accusative prons? It'd happen here
+    # plurals
+    mike = re.sub(r'(\S+NOUN\S+Plur\S+)',r'PLURAL_ADJ___ \1',lima)
+    # POSTPROCESSING
+    november = re.sub(r'(\S+_\S+)(_\S*_\S*_\S*)',r'\1',mike)
+    return november
 
-def np_ord():
-    tag = ord_prep(doc)[0]
+def np_ord(tag):
+    # tag = compulsory_subj(ord_prep(doc)[4])
     #
     # first remove DETs which are (in)definite articles
-    a = re.sub(r'el_DET ',r'',tag) # ,flags=re.MULTILINE
+    a = re.sub(r'(el|uno)_DET ',r'',tag) # ,flags=re.MULTILINE
     # move all DETs before NOUNs
     b = re.sub(r'(\S+_DET)(\s+)(\S+_NOUN)',r'\3\2\1',a)
     # modify tags for INDEF, POSS and DEM
@@ -95,12 +117,15 @@ def np_ord():
     g = re.sub(r'(\S+_IDF)(\s)(\S+_NUM)(\s)(\S+_NOUN)',r'\5\4\3\2\1',f)
     # now possessive constructions
     h = re.sub(r'(\S+_NOUN)(\s)(\S+_PSS)',r'\1\2PROPIO\2\3',g)
-    return h
+    i = re.sub(r'(\S+_ADP) ',r'',h) # "para" seems to be used in glosses
+    # fingerspell proper nouns (or include in OOV checker)
+    # POSTPROCESSING
+    j = re.sub(r'(\S+)(_\S+)',r'\1',i) # omit j if you want to pass POS to other functions
+    k = re.sub(r'_SPACE_',r'',j)
+    return k.upper()
 
-def plural_constructions(doc):
-    # this should come before reordering rules
+def oov_check():
     pass
-
 
 #%%
 '''
@@ -112,8 +137,29 @@ Alternative algorithm (a la Vegas Cañas):
 
 #%%
 
+
+#%%
 def main():
-    pass
+    model_load = importLM()
+    # infile = '/Users/e.mcgill/Downloads/resignontasknluoverspanish/frases_train_1.txt'
+    infile = sys.argv[1]
+    # outfile = 'test_output.txt'
+    outfile = sys.argv[2]
+    with open(infile, 'r', encoding='utf-8') as f, open(outfile, 'w') as w:
+        docs = []
+        preps = []
+        tags = []
+        outs = []
+        for line in f:
+            docs.append(model_load.to_doc(line))
+        for doc in docs:
+            preps.append(ord_prep(doc)[4])
+        for tag in preps:
+            tags.append(compulsory_subj(tag))
+        for item in tags:
+            outs.append(np_ord(item))
+        for line in outs:
+            w.write(line)
 
 if __name__ == '__main__':
     main()
