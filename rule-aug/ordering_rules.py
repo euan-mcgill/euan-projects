6ba224a930ec892a1cb2277861a636ee8ef6f944
Euan McGill <mcgilleuan@icloud.com>
2022-03-21 15:42:23 +0100

    rules completed current iteration

--- rule-aug/ordering_rules.py
+++ rule-aug/ordering_rules.py
@@ -8,12 +8,14 @@ Created on Fri Mar  4 17:48:58 2022
 
 Rules covered:
     NP order
-    Compulsory subject
+    Compulsory subject (usted is not included/dealt with separately)
     Movement of ADVs after the verb
     No articles
     No adpositions (might need to alter to accept 'para')
     Possessive constructions
-    Mark plurals with "PLURAL" marker
+    Mark plural nouns with "PLURAL" marker
+    Mark future and past tense verbs with "FUTURO"/"PASADO"
+    Lexical rules (Tener que -> Necesitar)
 """
 
 from model_init import importLM
@@ -22,7 +24,7 @@ import spacy as sp
 import sys
 
 # model_load = importLM()
-# doc = model_load.to_doc("de los treinta a los setenta años se renovará cada diez años")
+# doc = model_load.to_doc("de los treinta a los setenta años tienes que se renovará cada diez años")
 # testprondoc = model_load.to_doc("debes recoger el denei en el mismo sitio donde tú lo solicistaste")
 # doc = model_load.to_doc("una fotocopia del denei también vale si no tienes el original")
 # doc = model_load.to_doc("una fotocopia del denei ya no vale si tú no tienes el original")
@@ -51,7 +53,8 @@ def ord_prep(doc):
         pernum = (token.lemma_, token.pos_,
                   ' '.join(token.morph.get("Person")),
                   ' '.join(token.morph.get("Number")),
-                  ' '.join(token.morph.get("Case")))
+                  ' '.join(token.morph.get("Case")),
+                  ' '.join(token.morph.get("Tense")))
         # merge lemma and pos tag together as one lexical item, merge verb inflection info
         combine = '_'.join(tagged)
         inflect = '_'.join(pernum)
@@ -75,36 +78,39 @@ def compulsory_subj(mor):
     # another edge case - reflexive verbs (check)
     # mor = ord_prep(doc)[4]
     # Adverbs go after the verb apart from "no" or "ya no". Move other adverbs
-    alpha = re.sub(r'ya_ADV___', r'ya_YA___',mor)
-    bravo = re.sub(r'no_ADV___', r'no_NEG___',alpha)
-    bc = re.sub(r' Dat\b',r'',bravo)
+    alpha = re.sub(r'ya_ADV____', r'ya_YA____',mor)
+    bravo = re.sub(r'no_ADV____', r'no_NEG____',alpha)
+    bc = re.sub(r' (Dat|Nom)_\b',r'',bravo)
     charlie = re.sub(r'(\S+_ADV\S+)(\s)(\S+_(VERB|AUX)\S+)',r'\3\2\1',bc)
     # now to the pronouns
-    delta = re.sub(r'(\S+(VERB|AUX)_1_Sing_)',r'yo_PRON_1_Sing_Nom \1',charlie)
-    echo = re.sub(r'(\S+(VERB|AUX)_2_Sing_)',r'tú_PRON_2_Sing_Nom \1',delta)
+    delta = re.sub(r'(\S+(VERB|AUX)_1_Sing_)',r'yo_PRON_1_Sing_Nom_ \1',charlie)
+    echo = re.sub(r'(\S+(VERB|AUX)_2_Sing_)',r'tú_PRON_2_Sing_Nom_ \1',delta)
     # something to deal with ella, ellas
-    foxtrot = re.sub(r'(\S+(VERB|AUX)_3_(Sing|Plur)_)',r'él_PRON_3_Sing_Nom \1',echo)
-    golf = re.sub(r'(\S+(VERB|AUX)_1_Plur_)',r'nuestro_PRON_1_Sing_Nom \1',foxtrot)
-    hotel = re.sub(r'(\S+(VERB|AUX)_2_Plur_)',r'vuestro_PRON_2_Sing_Nom \1',golf)
+    foxtrot = re.sub(r'(\S+(VERB|AUX)_3_(Sing|Plur)_)',r'él_PRON_3_Sing_Nom_ \1',echo)
+    golf = re.sub(r'(\S+(VERB|AUX)_1_Plur_)',r'nuestro_PRON_1_Sing_Nom_ \1',foxtrot)
+    hotel = re.sub(r'(\S+(VERB|AUX)_2_Plur_)',r'vuestro_PRON_2_Sing_Nom_ \1',golf)
     # now move pron before ya no then no
     india = re.sub(r'(\S+NEG\S+)(\s)(\S+PRON\S+)',r'\3\2\1',hotel)
     juliett = re.sub(r'(\S+YA\S+)(\s)(\S+PRON\S+)',r'\3\2\1',india)
     # double prons, get rid of acc prons
-    kilo = re.sub(r'(\S+PRON\S+_Acc)',r'',juliett)
-    lima = re.sub(r'(\S+PRON\S+_Nom)(\s+)(\S+PRON\S+_Nom)',r'\1',kilo)
-    # future/past tense also here
-    # what should we do with accusative prons? It'd happen here
+    kilo = re.sub(r'(\S+(PRON|VERB)\S+Acc[_ ])',r'',juliett)
+    lima = re.sub(r'(\S+PRON\S+_Nom_)(\s+)(\S+PRON\S+_Nom_)',r'\1',kilo)
+    # future tense marker
+    mike = re.sub(r'(\S+(VERB|AUX)\S+Fut)',r'FUTURO_ADJ____ \1',lima)
+    # past tense marker - now remove particle?
+    november = re.sub(r'(\S+(VERB|AUX)\S+Past)',r'PASADO_ADJ____ \1',mike)
     # plurals
-    mike = re.sub(r'(\S+NOUN\S+Plur\S+)',r'PLURAL_ADJ___ \1',lima)
+    oscar = re.sub(r'(\S+NOUN\S+Plur\S+)',r'PLURAL_ADJ____ \1',november)
     # POSTPROCESSING
-    november = re.sub(r'(\S+_\S+)(_\S*_\S*_\S*)',r'\1',mike)
-    return november
+    papa = re.sub(r'(\S+_\S+)(_\S*_\S*_\S*_\S*)',r'\1',oscar)
+    quebec = re.sub(r'\s+',r' ',papa)
+    return quebec
 
 def np_ord(tag):
     # tag = compulsory_subj(ord_prep(doc)[4])
     #
     # first remove DETs which are (in)definite articles
-    a = re.sub(r'(el|uno)_DET ',r'',tag) # ,flags=re.MULTILINE
+    a = re.sub(r'\b(el|uno)_DET ',r'',tag)
     # move all DETs before NOUNs
     b = re.sub(r'(\S+_DET)(\s+)(\S+_NOUN)',r'\3\2\1',a)
     # modify tags for INDEF, POSS and DEM
@@ -112,39 +118,34 @@ def np_ord(tag):
     d = re.sub(r'(mi|tu|su|nuestro|vuestro)(_DET)',r'\1_PSS',c)
     e = re.sub(r'(cada|todo|mismo|otro|mucho|tanto|varios)(_DET)',r'\1_IDF',d)
     # all DEM + NOUNs are now flipped, 3 combos only, first DEM NUM NOUN -> NOUN DEM NUM
-    f = re.sub(r'(\S+_DEM)(\s)(\S+_NUM)(\s)(\S+_NOUN)',r'\5\4\1\2\3',e)
+    f = re.sub(r'(\S+_DEM)(\s)(\S+_NUM)(\s)(\S+_PLURAL)(\s)(\S+_NOUN)',r'\5\4\7\6\1\2\3',e)
+    g = re.sub(r'(\S+_DEM)(\s)(\S+_NUM)(\s)(\S+_NOUN)',r'\5\4\1\2\3',f)
     # now INDEF NUM NOUN -> NOUN NUM INDEF
-    g = re.sub(r'(\S+_IDF)(\s)(\S+_NUM)(\s)(\S+_NOUN)',r'\5\4\3\2\1',f)
+    h = re.sub(r'(\S+_IDF)(\s)(\S+_NUM)(\s)(PLURAL_\S+)(\s)(\S+_NOUN)',r'\5\4\7\6\3\2\1',g)
+    i = re.sub(r'(\S+_IDF)(\s)(\S+_NUM)(\s)(\S+_NOUN)',r'\5\4\3\2\1',h) # need to incorporate PLURAL_ADJ marker
     # now possessive constructions
-    h = re.sub(r'(\S+_NOUN)(\s)(\S+_PSS)',r'\1\2PROPIO\2\3',g)
-    i = re.sub(r'(\S+_ADP) ',r'',h) # "para" seems to be used in glosses
+    j = re.sub(r'(\S+_NOUN)(\s)(\S+_PSS)',r'\1\2PROPIO\2\3',i)
+    k = re.sub(r'(\S+_ADP) ',r'',j) # "para" seems to be used in glosses
     # fingerspell proper nouns (or include in OOV checker)
     # POSTPROCESSING
-    j = re.sub(r'(\S+)(_\S+)',r'\1',i) # omit j if you want to pass POS to other functions
-    k = re.sub(r'_SPACE_',r'',j)
-    return k.upper()
+    l = re.sub(r'(\S+)(_\S+)',r'\1',k) # omit j if you want to pass POS to other functions
+    # replace tener que with necesitar for more LSE-ish "need" construction
+    m = re.sub(r'tener que', r'necesitar',l)
+    n = re.sub(r'_SPACE__',r'',m)
+    return n.upper()
 
 def oov_check():
     pass
 
 #%%
-'''
-Alternative algorithm (a la Vegas Cañas):
-    Collect all words in a dict
-    Label
-    If they match conditional word order, plug them into sentence
-'''
-
-#%%
-
-
-#%%
 def main():
     model_load = importLM()
+    print("spaCy model loaded")
     # infile = '/Users/e.mcgill/Downloads/resignontasknluoverspanish/frases_train_1.txt'
     infile = sys.argv[1]
     # outfile = 'test_output.txt'
     outfile = sys.argv[2]
+    print("Augmenting data...")
     with open(infile, 'r', encoding='utf-8') as f, open(outfile, 'w') as w:
         docs = []
         preps = []
@@ -158,15 +159,9 @@ def main():
             tags.append(compulsory_subj(tag))
         for item in tags:
             outs.append(np_ord(item))
-        for line in outs:
-            w.write(line)
+        for l in outs:
+            w.write(l+'\n')
+    print("Augmented glosses created successfully!")
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
