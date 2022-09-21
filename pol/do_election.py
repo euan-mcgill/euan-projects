bd24e3eb11f208db2abdd69d9c38f4680e8cb84c
Euan McGill <mcgilleuan@icloud.com>
2022-09-21 15:09:24 +0200

    the latest

---
+++ pol/do_election.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 21 12:04:42 2022
+
+@author: e.mcgill
+"""
+
+from calcelec import calc_elec
+import pandas as pd
+
+# calculate = calc_elec.reg_party('electoral_calculus_data/2015.csv')
+
+six_election = ['electoral_calculus_data/1955.csv', 'electoral_calculus_data/1959.csv', 
+                 'electoral_calculus_data/1964.csv', 'electoral_calculus_data/1966.csv', 
+                 'electoral_calculus_data/1970.csv', 'electoral_calculus_data/1974f.csv', 
+                 'electoral_calculus_data/1974o.csv', 'electoral_calculus_data/1979.csv', 
+                 'electoral_calculus_data/1983.csv', 'electoral_calculus_data/1987.csv', 
+                 'electoral_calculus_data/1992.csv', 'electoral_calculus_data/1997.csv', 
+                 'electoral_calculus_data/2001.csv', 'electoral_calculus_data/2005.csv', ]
+
+eight_election = [ 'electoral_calculus_data/2010.csv', 'electoral_calculus_data/2015.csv', 
+                   'electoral_calculus_data/2017.csv', 'electoral_calculus_data/2019.csv', ]
+
+six_frames = []
+
+eight_frames = []
+
+for file in six_election:
+    with open(file, 'r') as elec:
+        result = calc_elec.reg_party(file,sixparty=True)
+        six_frames.append(result)
+
+for file in eight_election:
+    with open(file, 'r') as elec:
+        result = calc_elec.reg_party(file,sixparty=False)
+        eight_frames.append(result)
