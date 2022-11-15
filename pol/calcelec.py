2d574397db25b8b1ac4fc14e3d01f60323ede850
Euan McGill <mcgilleuan@icloud.com>
2022-11-15 09:20:27 +0100

    Eight party progress

--- pol/calcelec.py
+++ pol/calcelec.py
@@ -16,7 +16,7 @@ Function map:
 
 from iteround import saferound
 import pandas as pd
-# import sys
+import sys
 
 class CalcElec:
 
@@ -158,8 +158,114 @@ class CalcElec:
                             ld_votes, se_votes]
             return party_matrix
 
-        elif not self.sixparty:
-            pass
+        elif self.sixparty == False:
+            # 1 = Northern Ireland
+            uup_ni = self.data.loc[self.data['Area'] == 1, 'CON'].sum() # NI not yet done
+            sdl_ni = self.data.loc[self.data['Area'] == 1, 'LAB'].sum()
+            dup_ni = self.data.loc[self.data['Area'] == 1, 'LIB'].sum()
+            sif_ni = self.data.loc[self.data['Area'] == 1, 'NAT'].sum()
+            mnr_ni = self.data.loc[self.data['Area'] == 1, 'MIN'].sum()
+            oth_ni = self.data.loc[self.data['Area'] == 1, 'OTH'].sum()
+            ni_votes = [uup_ni, sdl_ni, dup_ni, sif_ni, mnr_ni, oth_ni]
+            # 2 = Scotland
+            con_sc = self.data.loc[self.data['Area'] == 2, 'CON'].sum()
+            lab_sc = self.data.loc[self.data['Area'] == 2, 'LAB'].sum()
+            lib_sc = self.data.loc[self.data['Area'] == 2, 'LIB'].sum()
+            brx_sc = self.data.loc[self.data['Area'] == 2, 'UKIP'].sum()
+            grn_sc = self.data.loc[self.data['Area'] == 2, 'Green'].sum()
+            nat_sc = self.data.loc[self.data['Area'] == 2, 'NAT'].sum()
+            mnr_sc = self.data.loc[self.data['Area'] == 2, 'MIN'].sum()
+            oth_sc = self.data.loc[self.data['Area'] == 2, 'OTH'].sum()
+            sc_votes = [con_sc, lab_sc, lib_sc, brx_sc, grn_sc, nat_sc, mnr_sc, oth_sc] # eight party progress so far
+            # 3 = North East
+            con_ne = self.data.loc[self.data['Area'] == 3, 'CON'].sum()
+            lab_ne = self.data.loc[self.data['Area'] == 3, 'LAB'].sum()
+            lib_ne = self.data.loc[self.data['Area'] == 3, 'LIB'].sum()
+            nat_ne = self.data.loc[self.data['Area'] == 3, 'NAT'].sum()
+            mnr_ne = self.data.loc[self.data['Area'] == 3, 'MIN'].sum()
+            oth_ne = self.data.loc[self.data['Area'] == 3, 'OTH'].sum()
+            ne_votes = [con_ne, lab_ne, lib_ne, nat_ne, mnr_ne, oth_ne]
+            # 4 = North West
+            con_nw = self.data.loc[self.data['Area'] == 4, 'CON'].sum()
+            lab_nw = self.data.loc[self.data['Area'] == 4, 'LAB'].sum()
+            lib_nw = self.data.loc[self.data['Area'] == 4, 'LIB'].sum()
+            nat_nw = self.data.loc[self.data['Area'] == 4, 'NAT'].sum()
+            mnr_nw = self.data.loc[self.data['Area'] == 4, 'MIN'].sum()
+            oth_nw = self.data.loc[self.data['Area'] == 4, 'OTH'].sum()
+            nw_votes = [con_nw, lab_nw, lib_nw, nat_nw, mnr_nw, oth_nw]
+            # 5 = Yorkshire and Humber
+            con_yh = self.data.loc[self.data['Area'] == 5, 'CON'].sum()
+            lab_yh = self.data.loc[self.data['Area'] == 5, 'LAB'].sum()
+            lib_yh = self.data.loc[self.data['Area'] == 5, 'LIB'].sum()
+            nat_yh = self.data.loc[self.data['Area'] == 5, 'NAT'].sum()
+            mnr_yh = self.data.loc[self.data['Area'] == 5, 'MIN'].sum()
+            oth_yh = self.data.loc[self.data['Area'] == 5, 'OTH'].sum()
+            yh_votes = [con_yh, lab_yh, lib_yh, nat_yh, mnr_yh, oth_yh]
+            # 6 = Cymru
+            con_cy = self.data.loc[self.data['Area'] == 6, 'CON'].sum()
+            lab_cy = self.data.loc[self.data['Area'] == 6, 'LAB'].sum()
+            lib_cy = self.data.loc[self.data['Area'] == 6, 'LIB'].sum()
+            nat_cy = self.data.loc[self.data['Area'] == 6, 'NAT'].sum()
+            mnr_cy = self.data.loc[self.data['Area'] == 6, 'MIN'].sum()
+            oth_cy = self.data.loc[self.data['Area'] == 6, 'OTH'].sum()
+            cy_votes = [con_cy, lab_cy, lib_cy, nat_cy, mnr_cy, oth_cy]
+            # 7 = West Midlands
+            con_wm = self.data.loc[self.data['Area'] == 7, 'CON'].sum()
+            lab_wm = self.data.loc[self.data['Area'] == 7, 'LAB'].sum()
+            lib_wm = self.data.loc[self.data['Area'] == 7, 'LIB'].sum()
+            nat_wm = self.data.loc[self.data['Area'] == 7, 'NAT'].sum()
+            mnr_wm = self.data.loc[self.data['Area'] == 7, 'MIN'].sum()
+            oth_wm = self.data.loc[self.data['Area'] == 7, 'OTH'].sum()
+            wm_votes = [con_wm, lab_wm, lib_wm, nat_wm, mnr_wm, oth_wm]
+            # 8 = East Midlands
+            con_em = self.data.loc[self.data['Area'] == 8, 'CON'].sum()
+            lab_em = self.data.loc[self.data['Area'] == 8, 'LAB'].sum()
+            lib_em = self.data.loc[self.data['Area'] == 8, 'LIB'].sum()
+            nat_em = self.data.loc[self.data['Area'] == 8, 'NAT'].sum()
+            mnr_em = self.data.loc[self.data['Area'] == 8, 'MIN'].sum()
+            oth_em = self.data.loc[self.data['Area'] == 8, 'OTH'].sum()
+            em_votes = [con_em, lab_em, lib_em, nat_em, mnr_em, oth_em]
+            # 9 = East Anglia
+            con_ea = self.data.loc[self.data['Area'] == 9, 'CON'].sum()
+            lab_ea = self.data.loc[self.data['Area'] == 9, 'LAB'].sum()
+            lib_ea = self.data.loc[self.data['Area'] == 9, 'LIB'].sum()
+            nat_ea = self.data.loc[self.data['Area'] == 9, 'NAT'].sum()
+            mnr_ea = self.data.loc[self.data['Area'] == 9, 'MIN'].sum()
+            oth_ea = self.data.loc[self.data['Area'] == 9, 'OTH'].sum()
+            ea_votes = [con_ea, lab_ea, lib_ea, nat_ea, mnr_ea, oth_ea]
+            # 10 = South West
+            con_sw = self.data.loc[self.data['Area'] == 10, 'CON'].sum()
+            lab_sw = self.data.loc[self.data['Area'] == 10, 'LAB'].sum()
+            lib_sw = self.data.loc[self.data['Area'] == 10, 'LIB'].sum()
+            nat_sw = self.data.loc[self.data['Area'] == 10, 'NAT'].sum()
+            mnr_sw = self.data.loc[self.data['Area'] == 10, 'MIN'].sum()
+            oth_sw = self.data.loc[self.data['Area'] == 10, 'OTH'].sum()
+            sw_votes = [con_sw, lab_sw, lib_sw, nat_sw, mnr_sw, oth_sw]
+            # 11 = London
+            con_ld = self.data.loc[self.data['Area'] == 11, 'CON'].sum()
+            lab_ld = self.data.loc[self.data['Area'] == 11, 'LAB'].sum()
+            lib_ld = self.data.loc[self.data['Area'] == 11, 'LIB'].sum()
+            nat_ld = self.data.loc[self.data['Area'] == 11, 'NAT'].sum()
+            mnr_ld = self.data.loc[self.data['Area'] == 11, 'MIN'].sum()
+            oth_ld = self.data.loc[self.data['Area'] == 11, 'OTH'].sum()
+            ld_votes = [con_ld, lab_ld, lib_ld, nat_ld, mnr_ld, oth_ld]
+            # 12 = South East
+            con_se = self.data.loc[self.data['Area'] == 12, 'CON'].sum()
+            lab_se = self.data.loc[self.data['Area'] == 12, 'LAB'].sum()
+            lib_se = self.data.loc[self.data['Area'] == 12, 'LIB'].sum()
+            nat_se = self.data.loc[self.data['Area'] == 12, 'NAT'].sum()
+            mnr_se = self.data.loc[self.data['Area'] == 12, 'MIN'].sum()
+            oth_se = self.data.loc[self.data['Area'] == 12, 'OTH'].sum()
+            se_votes = [con_se, lab_se, lib_se, nat_se, mnr_se, oth_se]
+
+            party_matrix = [ni_votes, sc_votes, ne_votes, nw_votes, yh_votes,
+                            cy_votes, wm_votes, em_votes, ea_votes, sw_votes,
+                            ld_votes, se_votes]
+            return party_matrix
+        
+        else:
+            print("Please specify whether analysis is 6 party or 8 party: \n (6 party = pre-2010)")
+            sys.exit()
 
     def electorate(self):
         total_votes = self.votes_per_party()
