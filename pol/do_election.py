d782e1e4a8740fdc13692d3ad3a8e14d1c2d05cd
Euan McGill <euan.mcgill@upf.edu>
2022-11-17 12:15:18 +0100

    Eight party complete

--- pol/do_election.py
+++ pol/do_election.py
@@ -9,11 +9,6 @@ Created on Thu Apr 21 12:04:42 2022
 from calcelec import CalcElec
 import pandas as pd
 
-calculate = CalcElec(infile='electoral_calculus_data/2005.csv', sixparty=True)
-
-test = calculate.dhondt_calc()
-#%%
-
 six_election = ['electoral_calculus_data/1955.csv', 'electoral_calculus_data/1959.csv', 
                  'electoral_calculus_data/1964.csv', 'electoral_calculus_data/1966.csv', 
                  'electoral_calculus_data/1970.csv', 'electoral_calculus_data/1974f.csv', 
@@ -30,14 +25,4 @@ six_frames = []
 
 eight_frames = []
 
-for file in six_election:
-    with open(file, 'r') as elec:
-        result = CalcElec.reg_party(file,sixparty=True,seat_total=630)
-        six_frames.append(result)
-
-for file in eight_election:
-    with open(file, 'r') as elec:
-        result = CalcElec.reg_party(file,sixparty=False)
-        eight_frames.append(result)
-
-#%%
+calculate = CalcElec(infile='electoral_calculus_data/2019.csv', sixparty=False).hamilton()
